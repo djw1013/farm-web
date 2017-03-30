ac2219777ce17a2f625825c71a259192dfa0eea1
16060834 <16060834@CNHQ-16060834N.sn.suning.ad>
2017-03-30 11:52:30 +0800

    djw 引入log4j

--- farm-web/src/main/java/com/wj/farm/web/controller/TestController.java
+++ farm-web/src/main/java/com/wj/farm/web/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.wj.farm.web.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +25,12 @@ public class TestController {
     @Autowired
     private ITestService iTestService;
     
+    private static Logger logger = LoggerFactory.getLogger(TestController.class);
+    
     @RequestMapping("/test")
     public void test(){
-        System.out.println(iTestService.queryList());
+        List<Map<String, Object>> list = iTestService.queryList();
+        logger.info("---------------------测试成功-----------------"+list);
     }
 
 }
